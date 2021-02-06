779a3aad3dbb55cdbd7cfc3d5ae5400e1e1e209c
Atharv-Joshi <joshiatharv1078@gmail.com>
2021-02-06 19:15:28 +0530

    files for heroku deployment

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"email@com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
